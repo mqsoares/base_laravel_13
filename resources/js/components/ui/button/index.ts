d6e8c861a6bc06fef4f9c73733d329425a2a3f23
Miquéias Soares <mqseraos@gmail.com>
2026-08-01 17:34:55 +0000

    feat: add button component variants

--- resources/js/components/ui/button/index.ts
+++ resources/js/components/ui/button/index.ts
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority"
 export { default as Button } from "./Button.vue"
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -19,6 +19,12 @@ export const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        warning:
+          "border bg-background shadow-xs text-warning hover:bg-warning-foreground hover:text-warning/80 dark:bg-input/30 dark:border-input dark:hover:bg-input/50 dark:hover:text-warning/50",
+        danger:
+          "border bg-background shadow-xs text-danger hover:bg-danger-foreground hover:text-danger/80 dark:bg-input/30 dark:border-input dark:hover:bg-input/50 dark:hover:text-danger/50",
+        info:
+          "border bg-background shadow-xs text-info-secondary hover:bg-info-foreground hover:text-info-secondary/80 dark:bg-input/30 dark:border-input dark:hover:bg-input/50 dark:hover:text-info-secondary/50",
       },
       size: {
         "default": "h-9 px-4 py-2 has-[>svg]:px-3",
